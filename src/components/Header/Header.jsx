97256381db2f6bf5b1473d04ab11662c8b56cfe6
Luiz Lopes <luizlopes12@outlook.com>
2022-02-01 13:13:29 -0300

    Estilos basicos com styled compnents

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,21 @@ import {Navbar} from './style'
 const Header = () => {
     return (
         <Container>
-        <div>Opa</div>
+        <Navbar>
+            <div>
+                <img src={require('../../img/logo.svg').default}/>
+                <span>Enver</span>
+            </div>
+            <div>
+                <a href="#">Home</a>
+                <a href="#">Services</a>
+                <a href="#">Our Project</a>
+                <a href="#">About us</a>
+            </div>
+            <div>
+                <button>Contact us</button>
+            </div>
+        </Navbar>
         </Container>
     );
 }
---
+++ src/components/Header/style.js
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+export const Navbar = styled.nav`
+    margin-top: 30px;
+    height: 60px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    div{
+        display: flex;
+        align-items: center;
+    }
+    a{
+        text-decoration: none;
+        color: rgba(255, 255, 255, .7);
+        margin: 0 10px;
+        transition: all .2s ease-in-out;
+        &:hover{
+            color: #5454D4;
+        }
+    }
+    span{
+        font-size: 28px;
+        margin-left: 8px;
+        font-weight: bold;
+    }
+    button{
+        background-color: transparent;
+        color: #fff;
+        padding: 10px 25px;
+        cursor: pointer;
+        font-size: 16px;
+        font-weight: 400;
+        border: 1px solid #fff;
+        border-radius: 5px;
+        transition: all .2s ease-in-out;
+        &:hover{
+            color: #333;
+            background-color: #f7f7f7;
+        }
+    }
+`;
