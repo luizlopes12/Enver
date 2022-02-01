f0ae67e982911dbe53d83686715b8736a8910c4e
Luiz Lopes <luizlopes12@outlook.com>
2022-02-01 12:42:54 -0300

    Update Container.js

--- src/styles/Container.js
+++ src/styles/Container.js
@@ -1,8 +1,13 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-    background-color: red;
+    height: 100px;
     width: 80%;
     margin: auto;
-    
+    @media(max-width: 768px){
+        width: 90%;
+    }
+    @media(max-width: 375px){
+        width: 95%;
+    }
 `;
