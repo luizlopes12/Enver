5ad54395b8a9b8d3fc57f1268212a2164834c88d
Luiz Lopes <luizlopes12@outlook.com>
2022-02-02 13:29:24 -0300

    menu e navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,14 +4,30 @@ import {Navbar} from './style'
 const Header = () => {
     const handleClick = () =>{
         let nav = document.querySelector('#nav')
-        nav.classList.contains('menu-closed')?
-        nav.classList.add('menu-open')
+        nav.classList.contains('nav-opened') ?
+        nav.classList.remove('nav-opened')
         :
-        nav.classList.add('menu-closed')
+        nav.classList.add('nav-opened')
     }
     return (
         <Container>
-            
+            <Navbar>
+                <div>
+                    <img src={require('../../img/logo.svg').default} alt="Logo" />
+                    <span>Enver</span>
+                </div>
+                    <i><img src={require('../../img/menu-icon.svg').default} onClick={handleClick} className='icon-open' alt="Menu icon" /></i>
+                <nav id="nav" className="nav-closed">
+                    <i><img src={require('../../img/close-menu-icon.svg').default} onClick={handleClick}  className='icon-close' alt="Menu icon" /></i>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">Our Project</a></li>
+                        <li><a href="#">About us</a></li>
+                    </ul>
+                    <button>Contact us</button>
+                </nav>
+            </Navbar>
         </Container>
     );
 }
--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -1,5 +1,89 @@
 import styled from "styled-components";
 
 export const Navbar = styled.nav`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 50px;
+    margin: 25px;
+    div, nav{
+        display: flex;
+        align-items: center;
+        span{
+            font-size: 1.6em;
+            margin: 0 5px;
+            font-weight: 600;
+        }
+    }
+    .icon-open{
+        display: none;
+    }
+    .icon-close{
+        filter: invert(100);
+        height: 20px;
+        display: none;
+    }
+    ul{
+        list-style: none;
+        display: flex;
+        li{
+            margin: 0 10px;
+            a{
+                text-decoration: none;
+                color: rgba(255,255,255, .7);
+            }
+        }
+    }
+    button{
+        background-color: transparent;
+        color: #FFF;
+        font-size: 1em;
+        border: 1px solid #FFF;
+        border-radius: 5px;
+        padding: 10px 15px;
+        margin: 0 0 0 10vw;
+    }
+
+
+    @media screen and (max-width: 768px){
+        nav{
+            position: absolute;
+            top: 0;
+            display: flex;
+            flex-direction: column;
+            height: 100vh;
+            width: 100vw;
+            align-items: center;
+            background-color: #000;
+            transition: all .2s linear;
+            ul{
+                display: flex;
+                flex-direction: column;
+                margin-top: 100px;
+                text-align: center;
+                li{
+                    margin: 20px;
+                }
+            }
+            button{
+                margin: 10px 0;
+            }
+        }
+        .nav-closed{
+            left: 100vw;
+        }
+        .nav-opened{
+            left: 0;
+        }
+        .icon-open{
+            display: initial;
+        }
+        .icon-close{
+            position: absolute;
+            top: 5vh;
+            right: 5vw;
+            display: initial;
+        }
+    }
 
 `;
