50051715aa02ef61d3ff8940d36d9bcf32c440fb
Luiz Lopes <luizlopes12@outlook.com>
2022-02-20 16:35:48 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import {GlobalStyle} from './styles/globalStyle'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Header from './components/Header/Header'
 function App() {
   return (
-    <div className="App">
+    <>
       <GlobalStyle/>
-      <Header/>
-    </div>
+      <Router basename={process.env.PUBLIC_URL}>
+        <Routes>
+          <Route path='/' element={<Header/>}/>
+        </Routes>
+      </Router>
+    </>
+
   );
 }
 
